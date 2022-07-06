821292fe694a5877a9adb78fd25065a6257c91f1
Denis Paluca <denispaluca@gmail.com>
2022-07-06 11:31:50 +0200

    Remove chainlinkClient from market upkeep

--- contracts/MarketUpkeep.sol
+++ contracts/MarketUpkeep.sol
@@ -1,41 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-// KeeperCompatible.sol imports the functions from both ./KeeperBase.sol and
-// ./interfaces/KeeperCompatibleInterface.sol
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 import "./IMarket.sol";
 
-contract MarketUpkeep is KeeperCompatibleInterface, ChainlinkClient {
-    using Chainlink for Chainlink.Request;
-
-    address public registryAddress = 0x4Cb093f226983713164A62138C3F718A5b595F73;
+contract MarketUpkeep is KeeperCompatibleInterface {
     uint32 public timeAskToBuy = 60;
     uint32 public timeBuyToInactive = 60;
     uint32 public timeInactiveToAsk = 60;
     uint32 public interval;
     uint256 public lastTimeStamp;
-    bytes32 private jobId;
-    uint256 private fee;
     IMarket market;
 
-    modifier isRegistry() {
-        require(
-            msg.sender == registryAddress,
-            "Only registry can perform upkeep"
-        );
-        _;
-    }
-
     constructor(address marketContractAddress, IMarket.Stage initStage) {
-        //CLIENT
-        setChainlinkToken(0xa36085F69e2889c224210F603D836748e7dC0088);
-        setChainlinkOracle(0x74EcC8Bdeb76F2C6760eD2dc8A46ca5e581fA656);
-        jobId = "ca98366cc7314957b8c012c72f05aeeb";
-        fee = (1 * LINK_DIVISIBILITY) / 10;
-
-        //KEEPER
         market = IMarket(marketContractAddress);
         lastTimeStamp = block.timestamp;
         if (initStage == IMarket.Stage.ASK) {
@@ -45,31 +22,6 @@ contract MarketUpkeep is KeeperCompatibleInterface, ChainlinkClient {
         } else {
             interval = timeInactiveToAsk;
         }
-    }
-
-    function requestPrice() public returns (bytes32 requestId) {
-        Chainlink.Request memory req = buildChainlinkRequest(
-            jobId,
-            address(this),
-            this.fulfillPrice.selector
-        );
-
-        // Set the URL to perform the GET request on
-        req.add("get", "https://price.deno.dev");
-
-        req.add("path", "price");
-        int256 timesAmount = 1;
-        req.addInt("times", timesAmount);
-
-        // Sends the request
-        return sendChainlinkRequest(req, fee);
-    }
-
-    function fulfillPrice(bytes32 _requestId, uint256 price)
-        public
-        recordChainlinkFulfillment(_requestId)
-    {
-        market.setMarketPrice(price);
     }
 
     function checkUpkeep(
@@ -88,7 +40,7 @@ contract MarketUpkeep is KeeperCompatibleInterface, ChainlinkClient {
 
     function performUpkeep(
         bytes calldata /* performData */
-    ) external override isRegistry {
+    ) external override {
         //We highly recommend revalidating the upkeep in the performUpkeep function
         if ((block.timestamp - lastTimeStamp) < interval) {
             return;
@@ -102,7 +54,6 @@ contract MarketUpkeep is KeeperCompatibleInterface, ChainlinkClient {
         } else if (stage == IMarket.Stage.BUY) {
             market.setStage(IMarket.Stage.INACTIVE);
             interval = timeInactiveToAsk;
-            requestPrice();
         } else {
             market.reset();
             interval = timeAskToBuy;
